b7890f765ace1bc93a3c2216abaad7ddc3bfbd2c
Mannie Cutean <68138888+manniecut@users.noreply.github.com>
2021-01-05 16:36:20 -0500

    database seeding

---
+++ sql-scripts/seed.recycl.sql
@@ -0,0 +1,72 @@
+BEGIN;
+TRUNCATE recyclables,
+feedback,
+pickups,
+users;
+
+RESTART IDENTITY CASCADE;
+
+INSERT INTO users (
+        username,
+        pass,
+        email,
+        userlocation
+    )
+VALUES (
+        'Mannie',
+        'mannie',
+        'mannie@mannie',
+        '1275 Woodward Ave, Detroit, MI 48226'
+    ), (
+        'Angie',
+        'angie',
+        'angie@angie',
+        '1275 Woodward Ave, Detroit, MI 48226'
+    ), (
+        'Zack',
+        'zack',
+        'zack@zack',
+        '1275 Woodward Ave, Detroit, MI 48226'
+    ), (
+        'Steve',
+        'steve',
+        'steve@steve',
+        '1275 Woodward Ave, Detroit, MI 48226'
+    ), (
+        'Marissa',
+        'marissa',
+        'marissa@marissa',
+        '1275 Woodward Ave, Detroit, MI 48226'
+    ), (
+        'Guest',
+        'guest',
+        'guest@guest',
+        '1275 Woodward Ave, Detroit, MI 48226'
+    );
+
+INSERT INTO recyclables (title)
+VALUES ('Batteries'),
+    ('Cosmetics'),
+    ('Electronics'),
+    ('Metal'),
+    ('Paper'),
+    ('Plastic');
+
+INSERT INTO feedback (msg, sender)
+VALUES (
+    'Hey! I love your app!',
+    1
+);
+
+INSERT INTO pickups (details, pickuplocation, pickupdate, client)
+VALUES (
+    'Batteries',
+    '1275 Woodward Ave, Detroit, MI 48226',
+    '2021-01-31, 17:30',
+
+    1
+);
+
+
+
+COMMIT;
