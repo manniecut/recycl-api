3d270b2dc3d6b2fa6776752976c474c62e39f31c
Mannie Cutean <68138888+manniecut@users.noreply.github.com>
2021-01-05 19:55:37 -0500

    feedback endpoints functional

--- src/feedback/feedback-router.js
+++ src/feedback/feedback-router.js
@@ -5,9 +5,67 @@ const feedbackRouter = express.Router();
 const jsonParser = express.json();
 
 feedbackRouter
-    .post('/', jsonParser, (req, res, next) =>{
-
+    .route('/')
+    .get((req, res, next) => {
+        FeedbackService.getAllFeedback(req.app.get('db'))
+            .then(feedback => {
+                res.json(feedback)
+            })
+            .catch(next)
+    })
+    .post(jsonParser, (req, res, next) => {
+        const { msg, sender } = req.body
+        const newFeedback = { msg, sender }
+        for (const [key, value] of Object.entries(newFeedback)) {
+            if (value == null) {
+                return res.status(400).json({
+                    error: { message: `Missing '${key}' in request body` }
+                })
+            }
+        }
+        FeedbackService.insertFeedback(
+            req.app.get('db'),
+            newFeedback
+        )
+            .then(feedback => {
+                res
+                    .status(201)
+                    .location(`/`)
+                    .json(feedback)
+            })
+            .catch(next)
     })
 
+feedbackRouter
+    .route('/:feedbackid')
+    .all((req, res, next) => {
+        FeedbackService.getFeedbackById(
+            req.app.get('db'),
+            req.params.feedbackid
+        )
+        .then(feedback => {
+            if (!feedback) {
+                return res.status(404).json({
+                    error: { message: `This feedback doesn't exist` }
+                })
+            }
+            res.feedback = feedback
+            next()
+        })
+        .catch(next)
+    })
+    .get((req, res, next) => {
+        res.json(res.feedback)
+    })
+    .delete((req, res, next) => {
+        FeedbackService.deleteFeedback(
+            req.app.get('db'),
+            req.params.feedbackid
+        )
+        .then(numRowsAffected => {
+            res.status(204).end()
+        })
+        .catch(next)
+    })
 
 module.exports = feedbackRouter;
--- src/feedback/feedback-service.js
+++ src/feedback/feedback-service.js
@@ -1,22 +1,37 @@
 const FeedbackService = {
 
-    // Get the user's info from DB
-    getUser: (db, username) => {
-      return (
-        db('users')
-          .where({ username })
-          .first()
-      );
-    },
-  
-    // Compare passwords
-    comparePasswords: (password, dbpass) => {
-      if (password === dbpass) {
-       return "match"
-      } else {
-        return "no match"
-      }
-    }
-  };
-  
-  module.exports = FeedbackService;
+  // retrieve all feedback from DB
+  getAllFeedback(knex) {
+    return knex
+      .select('*')
+      .from('feedback');
+  },
+
+  // add new feedback to DB
+  insertFeedback(knex, newFeedback) {
+    return knex
+      .insert(newFeedback)
+      .into('feedback')
+      .returning('*')
+      .then(rows => {
+        return rows[0]
+      });
+  },
+
+  // Get feedback info from DB
+  getFeedbackById(knex, id) {
+    return knex
+      .from('feedback')
+      .where('id', id)
+      .first()
+  },
+
+  // delete feedback by ID
+  deleteFeedback(knex, id) {
+    return knex('feedback')
+      .where({ id })
+      .delete();
+  }
+};
+
+module.exports = FeedbackService;
