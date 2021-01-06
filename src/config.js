3d7ca507951152ab83bc6199edaa987098c59f10
Mannie Cutean <68138888+manniecut@users.noreply.github.com>
2021-01-06 04:54:02 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 5432,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/guthub-server',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/recycldb',
 }
